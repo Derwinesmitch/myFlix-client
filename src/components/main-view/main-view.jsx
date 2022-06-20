e30cd5a041afd187339bba756d04c25e28a218c8
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2022-06-20 21:06:01 +0200

    testing for final task

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,28 +1,45 @@
 import React from 'react';
 import axios from 'axios';
 
+<<<<<<< Updated upstream
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
+=======
+import { connect } from 'react-redux';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+// #0
+import { setMovies } from '../../actions/actions';
+
+// we haven't written this one yet
+import MoviesList from '../movies-list/movies-list';
+// import { MovieCard } from '../movie-card/movie-card';
+>>>>>>> Stashed changes
 import { MovieView } from '../movie-view/movie-view';
 
-export class MainView extends React.Component {
+class MainView extends React.Component {
     constructor(){
         super();
         this.state = {
+<<<<<<< Updated upstream
             movies: [],
             selectedMovie: null,
             user: null
+=======
+            user: null,
+>>>>>>> Stashed changes
         };
     }
 
     componentDidMount(){
       axios.get('https://movieappcf.herokuapp.com/movies')
       .then(response => {
-        this.setState({
-          movies: response.data
-        });
+
+        this.props.setMovies(response.data);
       })
-      .catch(error => {
+      .catch(function (error) {
         console.log(error);
       });
     }
@@ -41,6 +58,7 @@ export class MainView extends React.Component {
 
 
     render() {
+<<<<<<< Updated upstream
         const { movies, selectedMovie } = this.state;
     
         if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
@@ -56,8 +74,88 @@ export class MainView extends React.Component {
               ))
             }
           </div>
+=======
+
+      let { movies } = this.props;
+      let { user } = this.state;
+    
+        return (
+          <Router>
+             <Navbar user={user} />
+              <Row className="main-view justify-content-md-center">
+                <Route exact path="/" render={() => {
+                  if (!user) return <Col>
+                    <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+                      </Col>
+                  if (movies.length === 0) return <div className="main-view" />;
+                 return <MoviesList movies={movies}/>;
+                  }} />
+
+                <Route path="/register" render={() => {
+                  if (user) return <Redirect to="/" />
+                  return <Col>
+                      <RegistrationView />
+                    </Col>
+                  }} />
+
+                <Route path="/movies/:movieId" render={({ match, history }) => {
+                   if (!user) return <Col>
+                   <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+                     </Col>
+                 if (movies.length === 0) return <div className="main-view" />;                 
+                  return <Col md={8}>
+                      <MovieView movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
+                    </Col>
+                  }} />
+                
+                <Route path="/directors/:name" render={({ match, history }) => {
+                    if (!user) return <Col>
+                    <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+                      </Col>                
+                 if (movies.length === 0) return <div className="main-view" />;
+                    return <Col md={8}>
+                      <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} onBackClick={() => history.goBack()} />
+                  </Col>
+                  }} />
+
+                <Route path="/genre/:name" render={({ match, history }) => {
+                   if (!user) return <Col>
+                   <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+                     </Col>                  
+                  if (movies.length === 0) return <div className="main-view" />;
+                    return <Col md={8}>
+                      <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} onBackClick={() => history.goBack()} />
+                    </Col>
+                  }} />
+
+                <Route path={`/users/${user}`} render={({history, match}) => {
+                    if (!user) return <Col>
+                    <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+                      </Col>                   
+                    if (movies.length === 0) return <div className="main-view" />;
+                      return <Col>
+                      <ProfileView history={histoy} movies={movies} user={user === match.params.username} onBackClick={() => history.goBack()} />
+                    </Col>
+                  }} />
+                
+                <Route path={`/user-update/${user}`} render={({match, history}) => {
+                     if (!user) return <Col>
+                     <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+                       </Col>                  
+                   if (movies.length === 0) return <div className="main-view" />;
+                      return <Col>
+                      <UserUpdate user={user} onBackClick={() => history.goBack()} />
+                    </Col>
+                  }} />              
+              </Row>
+            </Router>
+>>>>>>> Stashed changes
         );
       }
 }
 
-export default MainView;
+    let mapStateToProps = state => {
+      return { movies: state.movies }
+    }
+
+    export default connect(mapStateToProps, { setMovies } )(MainView);
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< Updated upstream
 import  MainView  from './components/main-view/main-view';
+=======
+import Container from 'react-bootstrap/Container';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import MainView from "./components/main-view/main-view";
+>>>>>>> Stashed changes
 
 import './index.scss';
+
+const store = createStore(moviesApp());
 
 class MyFlixApplication extends React.Component {
     render() {
         return (
+<<<<<<< Updated upstream
             <MainView />
+=======
+            <Provider store={store}>
+                <Container>
+                    <MainView /> 
+                </Container>
+            </Provider>
+>>>>>>> Stashed changes
         );
     }
 }
