68ef1971b6d62a9154ebc03ae9aa67e4b8cdb496
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2022-06-22 17:46:12 +0200

    this commit will fix the branch issue

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,83 +1,84 @@
 import React from 'react';
 import axios from 'axios';
-
-<<<<<<< Updated upstream
-import { LoginView } from '../login-view/login-view';
-import { MovieCard } from '../movie-card/movie-card';
-=======
-import { connect } from 'react-redux';
-
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-// #0
-import { setMovies } from '../../actions/actions';
-
-// we haven't written this one yet
-import MoviesList from '../movies-list/movies-list';
-// import { MovieCard } from '../movie-card/movie-card';
->>>>>>> Stashed changes
+import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
+import { Navbar } from '../navbar/navbar';
+import { DirectorView } from '../director-view/director-view';
+import { GenreView } from '../genre-view/genre-view';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-class MainView extends React.Component {
+export class MainView extends React.Component {
     constructor(){
         super();
         this.state = {
-<<<<<<< Updated upstream
             movies: [],
-            selectedMovie: null,
-            user: null
-=======
             user: null,
->>>>>>> Stashed changes
         };
     }
 
-    componentDidMount(){
-      axios.get('https://movieappcf.herokuapp.com/movies')
-      .then(response => {
 
-        this.props.setMovies(response.data);
+    componentDidMount(){
+     let accessToken = localStorage.getItem('token');
+     if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem('user')
+       });
+        this.getMovies(accessToken);
+       }
+    }
+  
+
+        onLoggedIn(authData)  {
+      console.log(authData);
+      this.setState({
+        user: authData.user.Username
+      });
+
+      localStorage.setItem('token', authData.token);
+      localStorage.setItem('user', authData.user.Username);
+      this.getMovies(authData.token);
+    }
+    
+    
+    getMovies(token) {
+      axios.get('https://movieappcf.herokuapp.com/movies', {
+        headers: { Authorization: `Bearer ${token}` }
       })
-      .catch(function (error) {
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
         console.log(error);
       });
     }
-   
-    setSelectedMovie(movie) {
-        this.setState({
-            selectedMovie: movie
-        });
-    }
 
-    onLoggedIn(user)  {
+
+    onRegistration(register){
       this.setState({
-        user
+          register,
       });
     }
 
 
+
+    onLoggedOut() {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      this.setState({
+        user: null
+      });
+    }
+
     render() {
-<<<<<<< Updated upstream
-        const { movies, selectedMovie } = this.state;
-    
-        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+        const { movies,  user } = this.state;
 
-        if (movies.length === 0) return <div className="main-view" />;
-    
-        return (
-          <div className="main-view">
-            {selectedMovie
-              ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-              : movies.map(movie => (
-                <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
-              ))
-            }
-          </div>
-=======
-
-      let { movies } = this.props;
-      let { user } = this.state;
+        
     
         return (
           <Router>
@@ -88,7 +89,11 @@ class MainView extends React.Component {
                     <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
                       </Col>
                   if (movies.length === 0) return <div className="main-view" />;
-                 return <MoviesList movies={movies}/>;
+                 return movies.map(m => (
+                    <Col md={3} key={m._id}>
+                      <MovieCard movie={m} />
+                    </Col>
+                  ))
                   }} />
 
                 <Route path="/register" render={() => {
@@ -149,13 +154,8 @@ class MainView extends React.Component {
                   }} />              
               </Row>
             </Router>
->>>>>>> Stashed changes
         );
       }
-}
-
-    let mapStateToProps = state => {
-      return { movies: state.movies }
     }
 
-    export default connect(mapStateToProps, { setMovies } )(MainView);
+export default MainView;
--- src/index.jsx
+++ src/index.jsx
@@ -1,37 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< Updated upstream
-import  MainView  from './components/main-view/main-view';
-=======
 import Container from 'react-bootstrap/Container';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import moviesApp from './reducers/reducers';
 import { devToolsEnhancer } from 'redux-devtools-extension';
 import MainView from "./components/main-view/main-view";
->>>>>>> Stashed changes
-
 import './index.scss';
 
-const store = createStore(moviesApp());
+const store = createStore(moviesApp, devToolsEnhancer());
 
 class MyFlixApplication extends React.Component {
     render() {
         return (
-<<<<<<< Updated upstream
-            <MainView />
-=======
             <Provider store={store}>
                 <Container>
                     <MainView /> 
                 </Container>
             </Provider>
->>>>>>> Stashed changes
         );
     }
 }
 
-
-const container = document.getElementsByClassName('app-container')[0];
+const container = document.getElementsByClassName('root')[0];
 
 ReactDOM.render(React.createElement(MyFlixApplication), container);
