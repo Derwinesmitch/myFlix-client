718eb6a0abcbb186011fc0a577ee2090957b9b4a
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2022-08-28 12:50:20 +0200

    fixing parcel error

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -24,7 +24,7 @@ function movies(state = [], action) {
 function user(state = [], action) {
   switch (action.type) {
     case SET_USER:
-      return action.value;
+      return action.value || localStorage.getItem('user');
     default:
       return state;
   }
